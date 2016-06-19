13c00ac74f948c3d32b327013234f88f3fce4c31
Art Burtch <artburtch@huskers.unl.edu>
2016-06-19 16:17:22 -0500

    changed host in prod env, and reformatted the email view

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,7 +14,7 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  config.action_mailer.default_url_options = { :host => 'https://glacial-basin-82754.herokuapp.com/' }
+  config.action_mailer.default_url_options = { :host => 'glacial-basin-82754.herokuapp.com/' }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
